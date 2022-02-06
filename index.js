9c643ba3469822fe90019522b7e77c21a9655472
Lcnvdl <programando.soft@gmail.com>
2022-02-06 14:23:43 -0300

    Package converted to app

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
+#!/usr/bin/env node
+
 const fs = require("fs");
-const fs = require("path");
-const settings = JSON.parse(fs.readFileSync(".publish-pack.json", "utf8"));
+const path = require("path");
 const CleanCSS = require('clean-css');
 const { minify } = require("terser");
 const { transformAsync } = require('@babel/core');
+
+//  File is the first argument if exists, else the default file
+
+let file = process.argv[2] || "./publish-pack.json";
+
+if (!fs.existsSync(file)) {
+  console.log("File not found: " + file);
+  process.exit(1);
+}
+
+const settings = JSON.parse(fs.readFileSync(file, "utf8"));
+
+process.chdir(path.dirname(file));
 
 async function parse() {
   if (settings.css) {
@@ -14,7 +28,13 @@ async function parse() {
       customCss += content + "\n\n";
     });
 
-    const finalCss = new CleanCSS({ format: 'beautify' }).minify(newStyle).styles;
+    const finalCss = new CleanCSS({ format: 'beautify' }).minify(customCss).styles;
+    const outDir = path.normalize(path.dirname(settings.css.out));
+
+    if (!fs.existsSync(outDir)) {
+      fs.mkdirSync(outDir, { recursive: true });
+    }
+
     fs.writeFileSync(settings.css.out, finalCss, "utf8");
   }
 
@@ -26,12 +46,18 @@ async function parse() {
       customJs += content + "\n\n";
     });
 
-    const { code } = await transformAsync(scriptCode, {
+    const { code } = await transformAsync(customJs, {
       presets: ['@babel/preset-env']
     });
 
     const minifiedResult = await minify(code);
     const finalJs = minifiedResult.code || code;
+
+    const outDir = path.dirname(settings.js.out);
+
+    if (!fs.existsSync(outDir)) {
+      fs.mkdirSync(outDir, { recursive: true });
+    }
 
     fs.writeFileSync(settings.js.out, finalJs, "utf8");
   }
